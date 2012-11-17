e5ee3921d0ef5e8cdf43fc078226778981120bde
Orde Saunders <orde@decadecity.net>
2012-11-17 11:20:10 +0000

    Added test for media queries.
    This test is working in the positive - not tested negative.
    
    Sublime has done a whitespace conversion.

--- plugins/DejaVu.php
+++ plugins/DejaVu.php
@@ -1,7 +1,7 @@
 <?php
 
 class DejaVu extends Plugin {
-	
+
 	public $profile;
 
 	public function query($plugin, $features, $useragent) {
@@ -14,7 +14,7 @@ class DejaVu extends Plugin {
 		} catch (Exception $e) {
 			exit('Caught exception: '.$e->getMessage()."\n");
 		}
-		
+
 		$entry = $data->xpath("device[@ua='$useragent']");
 		$device_profile = null;
 		foreach ($entry as $device) {
@@ -22,13 +22,13 @@ class DejaVu extends Plugin {
 				$profile_trust = 0;
 				foreach ($device->profile as $profile) {
 					if ((integer)$profile['count'] > $profile_trust) {
-						$device_profile = (string)$profile; 
+						$device_profile = (string)$profile;
 						$profile_trust = $profile['count'];
 					}
 				}
 			}
 		}
-		$this->profile =  json_decode($device_profile, true);;
+		$this->profile =  json_decode($device_profile, true);
 	}
 }
 
